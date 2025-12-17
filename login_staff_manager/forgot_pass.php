1905cfae2b19be4c905bd5ba79bbb18e021cb39b
long <long03042005>
2025-12-17 09:50:05 +0700

    thêm trang cho nhân viên, quản lí. thêm trang dang nhập. đổi icon trên web

---
+++ login_staff_manager/forgot_pass.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="../login_staff_manager/forgot_pass.css">
+    <title>Forgot Password</title>
+</head>
+<body>
+    <div class="wrapper">
+        <form action="../login_staff_manager/forgot_pass.php" method="POST">
+            <h1>Quên mật khẩu</h1>
+            <div class="login">
+                <div class="input-box">
+                    <input type="email" name="email" placeholder="Nhập email" required>
+                </div>
+                <button type="submit" class="btn">Tiếp tục</button>
+                <div class="register-link"><p><a href="./login_staff.php">Trang chủ</a></p></div>
+            </div>
+        </form>
+    </div>
+</body>
+</html>
---
+++ login_staff_manager/login_manager.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="../login_staff_manager/login.css">
+        <title>Login</title>
+    </head>
+    <body>
+        <h1>Login</h1>
+        
+            <div class="login">
+
+                <h3><span class="choose"><a href="../login_staff_manager/login_staff.php">Staff</a></span> | <span class="choose active"><a href="../login_staff_manager/login_manager.php">Manager</a></span></h3>
+
+                <div class="input">
+                    <input type="text" class="name" id="user" placeholder="username" required>
+                </div>
+
+                <div class="input">
+                    <input type="password" class="password" id="password" placeholder="password" required>
+                </div>
+
+                <div class="remember-forget">
+                    <label for=""><input type="checkbox">Remember password</label>
+                    <a href="../login_staff_manager/forgot_pass.php">Forgot password?</a>
+                </div>
+
+                <button type="submit" class="btn" name="submit">Login</button>
+            </div>
+    </body>
+</html>
